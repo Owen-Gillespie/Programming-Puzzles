4b5b3b56f70da2191f6ab30e27a2025059d0daae
Owen Gillespie <owenlgillespie@gmail.com>
2017-12-08 11:13:36 -0800

    Clean up day eight solution and fix bug introduced by last refactor

--- adventOfCode/Day8/dayeight.py
+++ adventOfCode/Day8/dayeight.py
@@ -1,25 +1,20 @@
 from collections import defaultdict
 def processInstructions(filename):
+    '''Takes in a file with instructions in the form
+        'reg op value if reg comparison value'. Executes the operations and
+        returns the maximum register value at the end of the program, and the maximum
+        register value ever reached.  Registers default to a value of 0'''
+
     with open(filename,'r') as f:
         instructions = f.readlines()
     maxval = 0
     regs = defaultdict(int)
     for instruction in instructions:
-        instruction = instruction.split()
-        reg = instruction[0]
-        inc =  1 if instruction[1] == 'inc' else -1
-        amt = int(instruction[2])
-        cond1 = instruction[4]
-        cond2 = instruction[6]
-        op = instruction[5]
-        if eval(str(regs[reg]) + op + cond2):
-            regs[reg] += inc * amt
-            #if inc:
-            #    regs[reg] = regs.get(reg,0) + amt
-            #else:
-            #    regs[reg] = regs.get(reg,0) - amt
+        reg, cmd, amt, _, cond_reg, op, cond_val = instruction.split()
+        inc =  1 if cmd == 'inc' else -1
+        if eval(str(regs[cond_reg]) + op + cond_val):
+            regs[reg] += inc * int(amt)
             maxval = max(maxval, regs[reg]) 
-    print(max(regs.values()))
-    print(maxval) 
+    return max(regs.values()), maxval 
 
 print(processInstructions('in.in'))
